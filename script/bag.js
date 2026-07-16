e58f12fee2e97e9c200b4be1592ac85ce6ba3ba0
신용민 <gva06158@gmail.com>
2026-07-16 16:47:56 +0900

    Add bag viewer with custom button style

--- script/bag.js
+++ script/bag.js
@@ -1,24 +1,43 @@
-function showMyBag() {
-    // 1. [Object가 담긴 Array] 객체들이 담긴 배열 데이터
-    var myBag = [
-        { name: "여권 ✈️", count: 1 },
-        { name: "스마트폰 📱", count: 2 },
-        { name: "지갑 💳", count: 1 }
-    ];
+  function showMyBag() {
+      // 소지품 객체가 담긴 배열
+      var myBag = [
+          {
+              name: "여권 ✈️",
+              count: 1
+          },
+          {
+              name: "스마트폰 📱",
+              count: 2
+          },
+          {
+              name: "지갑 💳",
+              count: 1
+          },
+          {
+              name: "보조 배터리 🔋",
+              count: 1
+          }
+      ];
 
-    // 2. 출력할 텍스트 기본 세팅
-    var resultText = "🎒 [내 가방 속 물품 목록]\n-----------------------\n";
+      var resultText =
+          "🎒 내 가방 속 물품 목록\n" +
+          "-----------------------\n";
 
-    // 3. ★ for...in 루프 활용
-    // myBag 배열의 인덱스(0, 1, 2...)를 순서대로 변수 i에 자동으로 넣어줍니다.
-    for (var i in myBag) {
-        // i가 알아서 방 번호가 되므로 기존과 동일하게 접근 가능합니다.
-        resultText = resultText + "- " + myBag[i].name + " : " + myBag[i].count + "개\n";
-    }
+      var totalCount = 0;
 
-    resultText = resultText + "-----------------------\n";
-    resultText = resultText + "총 물품 종류: " + myBag.length + "가지";
+      // 소지품 객체를 반복해서 출력
+      for (var i = 0; i < myBag.length; i++) {
+          resultText +=
+              "• " + myBag[i].name +
+              ": " + myBag[i].count + "개\n";
 
-    // 4. 알림창 출력
-    alert(resultText);
-}
+          totalCount += myBag[i].count;
+      }
+
+      resultText +=
+          "-----------------------\n" +
+          "물품 종류: " + myBag.length + "가지\n" +
+          "전체 수량: " + totalCount + "개";
+
+      alert(resultText);
+  }
